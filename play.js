e075143697a9d2485843d0d794b1fb12b53d27ea
Teenaelza <teena.thaliath@gmail.com>
2021-08-25 20:34:18 +0000

    send message to the server and export and modulerized the client function

--- play.js
+++ play.js
@@ -1,17 +1,3 @@
-const net = require ('net');
-const connect = function () {
-const conn = net.createConnection( {
-  host : '135.23.223.133',
-  port : 50542
-});
-conn.setEncoding('utf8');
-conn.on('data', (data) => {
-  console.log('Server says: ', data);
-});
-conn.on('connect', () => {
-  conn.write('Hello from TJ!');
-});
-return conn;
-};
+const {connect} = require ('./client.js');
 console.log("Connecting ...");
 connect();
