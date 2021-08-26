8c4d808892b116ba52c903a1060575c595ecf580
Teenaelza <teena.thaliath@gmail.com>
2021-08-26 04:03:36 +0000

    added message

--- client.js
+++ client.js
@@ -1,30 +1,17 @@
-const net = require ('net');
+const net = require("net");
 const connect = function () {
-  const up = "Move: up";
-  const down = "Move: down";
-  const left = "Move: down";
-  const right = "Move: down";
-  const conn = net.createConnection( {
-    host : '135.23.223.133',
-    port : 50542
+  const conn = net.createConnection({
+    host: "135.23.223.133",
+    port: 50542,
   });
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
   });
-  conn.on('connect', () => {
-    console.log('Successfully connected to game server');
-    conn.write('Name: TJK');
-    let iteration = 0;
-    const interval = setInterval(function(){
-      iteration++
-      conn.write(up);
-      if(iteration === 10) {
-        clearInterval(interval) 
-      }
-    }, 250)
-
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: TJK");
   });
   return conn;
 };
-module.exports = {connect};
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,19 +1,5 @@
-const {connect} = require ('./client.js');
+const { connect } = require("./client.js");
+const { setupInput } = require("./input.js");
 console.log("Connecting ...");
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function (Key) {
-    // \u0003 maps to ctrl+c input
-    if (Key === '\u0003') {
-      process.exit();
-    }
-};
-connect();
-setupInput();
+const conn = connect();
+setupInput(conn);
